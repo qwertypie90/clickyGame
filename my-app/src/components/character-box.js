ac88e813db1d582b3e3a7c9adbbc80a9b07daeac
Lindsey McDowell <Lindsey@Lindseys-MacBook-Pro.local>
2018-01-25 15:47:14 -0800

    workingIsh

--- my-app/src/components/character-box.js
+++ my-app/src/components/character-box.js
@@ -1,27 +1,16 @@
 import React, { Component } from 'react';
 import Gallery from './characters.js';
-import characterz from './characterz.json';
 import "../style/App.css";
 
+class CharacterBox extends Component { 
 
-class CharacterBox extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = { character: characterz };
-    }
-
-    componentDidMount() {
-        //this.setState({ character: characterz });
-    }
-
-    render () {
-    	return(
-			<div>
-				<Gallery character={this.state.character} />
-			</div>
-		);
-    }
+render() { 
+	return (
+		<div> 
+			<Gallery /> 
+		</div>
+		); 
+	} 
 }
 
 export default CharacterBox;
--- my-app/src/components/characters.js
+++ my-app/src/components/characters.js
@@ -1,22 +1,54 @@
 import React, { Component } from 'react';
-
+import characterz from './characterz.json';
 
 class Gallery extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { character: [] };
+    }
+
+    componentDidMount() {
+        this.setState({ character: characterz });
+    }
+
+    /**
+     * Array shuffler found here https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+     * @param {*} values 
+     */
+    shuffle(values) {
+        for (let i = values.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [values[i], values[j]] = [values[j], values[i]];
+        }
+
+        return values;
+    }
+
+    handleClick(index) {
+        console.log("THIS WAS CLICKED", this.state.character[index])
+        // To change image of clicked box
+        const currentCharacterInState = this.state.character.slice();
+        //currentCharacterInState[index]['image'] = "http://via.placeholder.com/350x150";
+        const suffledCharacter = this.shuffle(currentCharacterInState);
+        this.setState({character: suffledCharacter});
+    }
+
     render() {
-        let char = this.props.character;
+        let characters = this.state.character;
+        console.log(characters)
 
         // const characterz = this.props; 
         // destructure the props with a default (not strictly necessary, but more convenient) 
-            return (
-                char.map(([key, item]) => {
-                    return (
-                        <div className="box" >
-    <img data-name ={key} alt={item.name} src={item.image} height= "190px" width="190px" />
-                        </div>
-                    )
-                })
-            )
-
+        return (
+            characters.map((character, index) => {
+                return(
+                    <div key={index}  onClick={() => this.handleClick(index)} className="box">
+                        <img alt={character.name} src={character.image} height="190px" width="190px" />
+                    </div>
+                )
+            })
+        )
     }
 }
 
